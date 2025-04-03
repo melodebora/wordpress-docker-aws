59d33f69a96507cb1a08430b4e1fce0b1af12cb1
Débora <90116812+melodebora@users.noreply.github.com>
2025-04-03 17:13:29 -0300

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -9,11 +9,11 @@ sudo service docker start
 sudo systemctl enable docker.service
 sudo usermod -aG docker ec2-user
 
-# Instala o Docker Compose
+# aqui eu instalo o Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-# Monta o Amazon EFS
+# faço a montagem no Amazon EFS
 sudo mkdir -p /wordpress
 sudo mount -t efs -o tls fs-00d1605a6207546e8:/ /wordpress
 
